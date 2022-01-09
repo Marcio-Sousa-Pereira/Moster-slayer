214559ea457ef89e04e3b5216b93681260c62d3a
marcio <marcio.sousa@g4flex.com.br>
2022-01-09 14:35:58 -0300

    feat: adicionando funcionalidades de ataque, defesa, e cura do jogador

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ new Vue({
   el: '#app',
   data: {
     running: false,
-    playerLife: 20,
+    playerLife: 100,
     monsterLife: 100,
+    logs: []
   },
 
   computed: {
@@ -13,10 +14,54 @@ new Vue({
   },
 
   watch: {
-
+    hasResult(value) {
+      if(value) {
+        this.running = false;
+      }
+    }
   },
 
   methods: {
+    startGame() {
+      this.running = true
+      this.playerLife = 100
+      this.monsterLife = 100
+      this.logs = []
+    },
 
+    attack(especial) {  
+      this.hurt('playerLife', 5, 10, false, "Monstro", "Jogador", 'monster');
+      if(this.monsterLife > 0){
+        this.hurt('monsterLife', 7, 12, especial, "Jogador", "Monstro", 'player');
+      }
+    },
+
+    hurt(atr, min, max, especial, source, target, cls) {
+      const plus = especial ? 5 : 0;
+      const hurt = this.getRandom(min + plus, max + plus);
+      this[atr] = Math.max(this[atr] - hurt, 0)
+
+      this.registerLogs(`${source} atingiu o ${target} com ${hurt}`, cls)
+    },
+
+    healAndHurt() {
+      this.heal(10, 15);
+      this.hurt('playerLife', 7, 12, false, "Monstro", "Jogador", 'monster');
+    },
+
+    registerLogs(text, cls) {
+      this.logs.unshift({ text, cls }) //unshift serve para colocar o parametro sempre em primeiro lugar
+    },
+
+    heal(min, max) {
+      const life = this.getRandom(min, max);
+      this.playerLife = Math.min(this.playerLife + life, 100);
+      this.registerLogs(`jogador ganhou força de ${life}`, 'player');
+    },
+
+    getRandom(min, max) {
+      const value = Math.random() * (max - min) + min
+      return Math.round(value);
+    }
   }
 })
